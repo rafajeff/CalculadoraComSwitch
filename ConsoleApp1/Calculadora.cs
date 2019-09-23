2f9404ea0eab91a8a2277740da255dc15f9b056e
rafael jeferson <rafajeferson@hotmail.com>
2019-09-22 23:24:16 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    static class Calculadora
+    {
+        /// <summary>
+        /// Soma dois números
+        /// </summary>
+        /// <param name="n1">Número 1</param>
+        /// <param name="n2">Numéro 2</param>
+        /// <returns>Retorna a soma de dois números</returns>
+        public static int Somar(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string op;
+            int n1, n2, resultado;
+
+            Console.WriteLine("Digite a operação que deseja realizar");
+            op = Console.ReadLine();
+            Console.WriteLine("Digite o primeiro numero");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero");
+            n2 = int.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+                case "+":
+                    resultado = Calculadora.Somar(n1, n2);
+                    Console.WriteLine($"A soma de {n1} + {n2} é: {resultado}");
+                    break;
+
+                default:
+                    Console.WriteLine("Operação Inválida");
+                    break;
+            }
+
+
+        }
+    }
+}
